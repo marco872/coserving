da957db152f74c5b98f8fcdd09f20b2a57033993
marco 111 <mrc.carrano@gmail.com>
2021-07-22 17:53:20 +0200

    models webinvestors and project

--- co_servings/admin.py
+++ co_servings/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Webinvestor, Project
+
+admin.site.register(Webinvestor)
+admin.site.register(Project)
--- co_servings/models.py
+++ co_servings/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+class Webinvestor(models.Model):
+	name = models.CharField(max_length=200, null=True)
+	phone = models.CharField(max_length=200, null=True)
+	email = models.CharField(max_length=200, null=True)
+	date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+	def __str__(self):
+		return self.name
+
+class Project(models.Model):
+	CATEGORY = (
+		('Guestroom', 'Guestroom'),
+		('Display area', 'Dispaly area'),
+		('Working station', 'Working station'),
+		('Office', 'Office'),
+		('Noursing room', 'Noursing room'),
+		('Food corner', 'Food corner'),
+		)
+
+	property_price = models.FloatField(null=True)
+	location = models.CharField(max_length=200, null=True)
+	dimension = models.CharField(max_length=200, null=True)
+	category = models.CharField(max_length=200, null=True, choices=CATEGORY)
+	building = models.CharField(max_length=200, null=True) #building cost+design+documentation and approval
+	typology = models.CharField(max_length=200, null=True)
+	total_project_price = models.FloatField(null=True) #project price+building price
+	date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+
+	
+	def __str__(self):
+		return self.location
+
+
