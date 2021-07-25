27162be47166d8e32cebebe2b416573cab8578ef
marco 111 <mrc.carrano@gmail.com>
2021-07-25 12:48:07 +0200

    settings

--- co_serving/settings.py
+++ co_serving/settings.py
@@ -137,27 +137,3 @@ STATICFILES_DIRS =[
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-def pytest_configure(config):
-    from django.conf import settings
-
-    settings.configure(
-        AUTHENTICATION_BACKENDS=[
-            "tests.base.FooPasswordBackend",
-            "tests.base.StubPasswordBackend",
-        ],
-        DEBUG=True,
-        DATABASE_ENGINE="sqlite3",
-        DATABASES={
-            "default": {
-                "NAME": ":memory:",
-                "ENGINE": "django.db.backends.sqlite3",
-                "TEST_NAME": ":memory:",
-            },
-        },
-        DATABASE_NAME=":memory:",
-        TEST_DATABASE_NAME=":memory:",
-        INSTALLED_APPS=INSTALLED_APPS,
-        MIDDLEWARE_CLASSES=MIDDLEWARE_CLASSES,
-        PASSWORD_HASHERS=["django.contrib.auth.hashers.MD5PasswordHasher"],
-        ROOT_URLCONF="tests.urls",
-    ) 
